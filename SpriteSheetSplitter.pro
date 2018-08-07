e859e64bc11b67987703344003d49e5e0f287e5c
Keven Chaussé <kevenchausse@gmail.com>
2018-08-07 00:00:22 -0400

    Ajout d'un icone, finalisation, translation, etc

--- SpriteSheetSplitter.pro
+++ SpriteSheetSplitter.pro
@@ -20,6 +20,10 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+TRANSLATIONS = spriter_fr.ts
+
+RC_FILE = myapp.rc
+
 CONFIG += c++11
 
 SOURCES += \
---
+++ spriter_fr.ts
@@ -0,0 +1,165 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>InputTab</name>
+    <message>
+        <location filename="inputtab.cpp" line="154"/>
+        <source>Manual Mode</source>
+        <translation>Mode Manuel</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="200"/>
+        <source>Refresh</source>
+        <translation>Rafraichir</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="201"/>
+        <source>Remove</source>
+        <translation>Retirer</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="202"/>
+        <source>Clear</source>
+        <translation>Effacer</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="203"/>
+        <source>ClearAll</source>
+        <translation>Effacer Tout</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="204"/>
+        <source>Apply To All</source>
+        <translation>Appliquer à tous</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="206"/>
+        <source>Automatic</source>
+        <translation>Automatique</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="208"/>
+        <source>Manual</source>
+        <translation>Manuel</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="239"/>
+        <source>SpriteSheet</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="240"/>
+        <source>Selection</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="264"/>
+        <source>Rows</source>
+        <translation>Rangées</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="266"/>
+        <source>Columns</source>
+        <translation>Colonnes</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="268"/>
+        <source>Current Output</source>
+        <translation>Indexe</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="270"/>
+        <source>Row Offset</source>
+        <translation>Offset de Rangée</translation>
+    </message>
+    <message>
+        <location filename="inputtab.cpp" line="272"/>
+        <source>Column Offset</source>
+        <translation>Offset de Colonne</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Spriter - by mooviies</source>
+        <translation>Spriter - par mooviies</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="25"/>
+        <source>Input</source>
+        <translation>Entrées</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="33"/>
+        <source>Add...</source>
+        <translation>Ajouter...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <source>Clear</source>
+        <translation>Effacer</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <source>Preview</source>
+        <translation>Résultat</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="104"/>
+        <source>Rows</source>
+        <translation>Rangées</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="121"/>
+        <source>Columns</source>
+        <translation>Colonnes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="138"/>
+        <source>Export</source>
+        <translation>Exporter</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="16"/>
+        <source>All files (*.*)</source>
+        <translation>Tous les fichiers (*.*)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="17"/>
+        <source>PNG (*.png)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="18"/>
+        <source>TIFF (*.tif)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="19"/>
+        <source>JPEG (*.jpg *.jpeg)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="20"/>
+        <source>BMP (*.bmp)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="61"/>
+        <source>Add spritesheet(s)</source>
+        <translation>Ajouter des Spritesheets</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="162"/>
+        <source>Output </source>
+        <translation>Sortie </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="199"/>
+        <source>Export spritesheet(s)</source>
+        <translation>Exporter les spritesheets</translation>
+    </message>
+</context>
+</TS>
